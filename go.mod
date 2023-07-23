365985d44f76b64bea4fb34cb64d27ea6ece8bb6
tmgasek <tomaszgasek28@gmail.com>
2023-07-23 10:39:05 +0100

    add stdin

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/tmgasek/go-wc
 
 go 1.20
 
-require github.com/spf13/pflag v1.0.5 // indirect
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -38,25 +39,41 @@ func main() {
 	var bytesFlag bool
 	pflag.BoolVarP(&linesFlag, "lines", "l", false, "count lines")
 	pflag.BoolVarP(&wordsFlag, "words", "w", false, "count words")
-	pflag.BoolVarP(&charsFlag, "chars", "c", false, "count bytes")
-	pflag.BoolVarP(&bytesFlag, "bytes", "m", false, "count chars")
+	pflag.BoolVarP(&bytesFlag, "chars", "c", false, "count bytes")
+	pflag.BoolVarP(&charsFlag, "bytes", "m", false, "count chars")
 	pflag.Parse()
+
+	// if no flags, default to -lwc
+	if !linesFlag && !wordsFlag && !charsFlag && !bytesFlag {
+		linesFlag = true
+		wordsFlag = true
+		bytesFlag = true
+	}
 
 	args := pflag.Args()
 
-	if (len(args) == 0) || (len(args) > 2) {
-		log.Fatal("Usage: wc [-clmw] [file]")
+	var data []byte
+	var err error
+	var file string
+
+	if len(args) == 0 {
+		fmt.Println("Usage: [OPTIONS] FILE")
+		fmt.Println("Count lines, words, characters, and bytes.")
+		fmt.Println("If FILE is - read from standard input.")
+		fmt.Println("Options:")
+		pflag.PrintDefaults()
+		os.Exit(1)
 	}
 
-	if len(args) != 1 {
-		log.Fatal("expected one file")
-
+	// if no file or file is -, read from stdin
+	if args[0] == "-" {
+		data, err = io.ReadAll(os.Stdin)
+	} else {
+		// read in file
+		file = args[0]
+		data, err = os.ReadFile(file)
 	}
 
-	file := args[0]
-
-	// read in file
-	data, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatal(err)
 	}
